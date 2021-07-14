91b963c6e070751a9fbd9ace7d9be58a74666338
Artiom Suqiasyan <artiom.suqiasyan1401@gmail.com>
2021-07-14 14:52:18 +0400

    fix: Fixed weather photos

--- Weather/my-app/src/Weather.jsx
+++ Weather/my-app/src/Weather.jsx
@@ -1,47 +1,51 @@
 import React from "react";
+import img1 from "./img/01.svg";
+import img2 from "./img/05.svg";
+import img3 from "./img/17.svg";
+import img4 from "./img/09.svg";
 
 export default function Weather() {
   const weatherInfo = [
     {
       id: 1,
       weekDey: "Monday",
-      imgURL: "./img/01.svg",
+      imgURL: img1,
       temp: "36C",
     },
     {
       id: 2,
       weekDey: "Tuesday",
-      imgURL: "./img/05.svg",
+      imgURL: img1,
       temp: "32C",
     },
     {
       id: 3,
       weekDey: "Wednesday",
-      imgURL: "./img/09.svg",
+      imgURL: img2,
       temp: "35C",
     },
     {
       id: 4,
       weekDey: "Thursday",
-      imgURL: "./img/17.svg",
+      imgURL: img2,
       temp: "45C",
     },
     {
       id: 5,
       weekDey: "Friday",
-      imgURL: "./img/09.svg",
+      imgURL:img3,
       temp: "36C",
     },
     {
       id: 6,
       weekDey: "Saturday",
-      imgURL: "./img/17.svg",
+      imgURL: img4,
       temp: "37C",
     },
     {
       id: 7,
       weekDey: "Sunday",
-      imgURL: "img/17.svg",
+      imgURL: img3,
       temp: "39C",
     },
   ];
@@ -50,7 +54,7 @@ export default function Weather() {
       <h2>
         {el.weekDey}
       </h2>
-      <img src={el.imgURL} alt="" />
+      <img src={el.imgURL} alt="WeatherPhoto" />
       <p>
         {el.temp}
       </p>
